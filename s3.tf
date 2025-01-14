29886b53e30c0b7d93e94a958b87d85c9e3c540f
Sachin <sc626361@gmail.com>
2025-01-14 11:22:22 +0530

    s3 code

--- s3.tf
+++ s3.tf
@@ -6,7 +6,12 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   bucket = "sachin-web-12890"
   acl    = "private"
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+  block_public_acls = false
   ignore_public_acls = false
+  block_public_policy = false
+  restrict_public_buckets = false
   website = {
     index_document = "index.html"
   }
@@ -21,6 +26,7 @@ resource "aws_s3_object" "object" {
   key          = "index.html"
   source       = "index.html"
   content_type = "text/html"
+  acl = "public-read"
   depends_on = [ module.s3_bucket ]
 
 }
